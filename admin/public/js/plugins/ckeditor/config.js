a47314d51de32cdd2ffb010d1e40762bfb375eb4
Vu Long Vu <longvuit18@gmail.com>
2021-04-24 08:36:28 +0700

    find and add rich editor lib (Vu)

---
+++ admin/public/js/plugins/ckeditor/config.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    config.allowedContent = true;
+    config.filebrowserBrowseUrl = 'public/plugins/ckfinder/ckfinder.html';
+    config.filebrowserUploadUrl = 'public/plugins/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+
+};
+
+
